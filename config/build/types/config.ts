6b6e053782f4497070eeff34e97b8c198b6d61e2
Mikhail Barkov <barkov.mihail@gmail.com>
2023-01-31 22:21:31 +0300

    Декомпозиция конфига. Опции конфигурации

---
+++ config/build/types/config.ts
@@ -0,0 +1,13 @@
+export type BuildType = 'production' | 'development'
+
+export interface BuildPaths {
+    entry: string,
+    build: string,
+    html: string
+}
+
+export interface BuildOptions {
+    mode: BuildType;
+    paths:BuildPaths;
+    isDev: boolean;
+}
