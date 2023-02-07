eee25d9bc8248a417618af87d1f7feeddaf29a4d
Mikhail Barkov <barkov.mihail@gmail.com>
2023-02-07 17:25:02 +0300

    AppRouter. Конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { classNames } from '../shared/lib/classNames/classNames';
 import {useTheme} from "./providers/ThemeProvider";
 import {AboutPage} from "pages/AboutPage";
 import { MainPage } from 'pages/MainPage';
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -16,12 +17,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage/>} />
-                <Route path={'/'} element={<MainPage />}/>
-            </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
