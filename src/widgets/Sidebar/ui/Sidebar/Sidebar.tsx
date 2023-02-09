d3db81571bad5dffbad4406c66b9f36b4a529e62
Mikhail Barkov <barkov.mihail@gmail.com>
2023-02-09 21:19:45 +0300

    i18n Интернационализация. Define plugin. Плагин для переводов

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Sidebar.module.scss';
 import React, {useState} from "react";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
@@ -21,6 +22,7 @@ export const Sidebar = ({className}: SidebarProps) => {
             <button onClick={onToggle}>Toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
