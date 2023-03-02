a2269f8a0da73948270e336d452d5f12b909ae56
Mikhail Barkov <barkovm@yahoo.com>
2023-03-02 12:17:06 +0300

    fix warnings

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -43,6 +43,7 @@ export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
     }, []);
 
     return (
+        // eslint-disable-next-line
         <>
             {children}
         </>
