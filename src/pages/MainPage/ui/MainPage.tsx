21c1c798973f59ea4900e347c26b75d4fcab18df
Mikhail Barkov <barkovm@yahoo.com>
2023-05-04 00:00:44 +0400

    add appImage, fallback, errorFallback

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '../../../widgets/Page/ui/Page/Page';
-import { Counter } from '../../../entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
         <Page>
-            <Counter />
             {t('Главная страница')}
         </Page>
     );
--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -2,12 +2,17 @@ import { useTranslation } from 'react-i18next';
 import { CSSProperties, useMemo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Avatar.module.scss';
+import { AppImage } from '../AppImage';
+import UserIcon from '../../assets/icons/avatar-20-20.svg';
+import { Icon } from '../Icon';
+import { Skeleton } from '../Skeleton';
 
 interface AvatarProps {
     className?: string;
     src?: string;
     size?: number;
     alt?: string;
+    fallbackInverted?: boolean;
 }
 
 export const Avatar = (props: AvatarProps) => {
@@ -16,17 +21,23 @@ export const Avatar = (props: AvatarProps) => {
     const {
         className,
         src,
-        size,
+        size = 100,
         alt,
+        fallbackInverted,
     } = props;
 
     const styles = useMemo<CSSProperties>(() => ({
-        width: size || 100,
-        height: size || 100,
+        width: size,
+        height: size,
     }), [size]);
 
+    const fallback = <Skeleton width={size} height={size} border="50%" />;
+    const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />;
+
     return (
-        <img
+        <AppImage
+            fallback={fallback}
+            errorFallback={errorFallback}
             alt={alt}
             src={src}
             style={styles}
