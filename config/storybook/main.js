a8a0e22d987e01e7246c381688fc4f4f48db44d4
Mikhail Barkov <barkovm@yahoo.com>
2023-05-09 10:35:03 +0300

    upgrade storybook

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,21 +1,16 @@
 module.exports = {
-    stories: [
-        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        {
-            name: '@storybook/addon-essentials',
-            options: {
-                background: false,
-            },
-        },
-        '@storybook/addon-interactions',
-        'storybook-addon-mock/register',
-        'storybook-addon-themes',
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-webpack5',
-    },
+  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', {
+    name: '@storybook/addon-essentials',
+    options: {
+      background: false
+    }
+  }, '@storybook/addon-interactions', 'storybook-addon-mock', 'storybook-addon-themes'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
