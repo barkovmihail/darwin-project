e990e5a0a8e1c9faed1a30f0c7c2e920b08cd920
Mikhail Barkov <barkovm@yahoo.com>
2023-06-05 12:39:55 +0400

    redesign modal, drawer

--- src/shared/ui/redesigned/Modal/Modal.tsx
+++ src/shared/ui/redesigned/Modal/Modal.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import { useModal } from '@/shared/lib/hooks/useModal/useModal';
-import { Overlay } from '../../redesigned/Overlay/Overlay';
+import { Overlay } from '../Overlay/Overlay';
 import cls from './Modal.module.scss';
-import { Portal } from '../../redesigned/Portal/Portal';
+import { Portal } from '../Portal/Portal';
+import { toggleFeatures } from '@/shared/lib/features';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 
 const ANIMATION_DELAY = 300;
 
@@ -15,12 +17,10 @@ interface ModalProps {
     lazy?: boolean;
 }
 
-/**
- * Компонент устарел, используйте новые компоненты из папки redesigned
- * @deprecated
- */
 export const Modal = (props: ModalProps) => {
     const { className, children, isOpen, onClose, lazy } = props;
+
+    const { theme } = useTheme();
 
     const { close, isClosing, isMounted } = useModal({
         animationDelay: ANIMATION_DELAY,
@@ -38,8 +38,19 @@ export const Modal = (props: ModalProps) => {
     }
 
     return (
-        <Portal>
-            <div className={classNames(cls.modal, mods, [className])}>
+        <Portal element={document.getElementById('app') ?? document.body}>
+            <div
+                className={classNames(cls.modal, mods, [
+                    className,
+                    theme,
+                    'app_modal',
+                    toggleFeatures({
+                        name: 'isAppRedesigned',
+                        on: () => cls.modalNew,
+                        off: () => cls.modalOld,
+                    }),
+                ])}
+            >
                 <Overlay onClick={close} />
                 <div className={cls.content}>{children}</div>
             </div>
